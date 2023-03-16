94915590ec803218a0d3dd1290747ac01cebc683
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2023-03-16 19:19:21 +0100

    feat: groups in dashboard.

---
+++ alumni-network-ui/src/api/group.js
@@ -0,0 +1,22 @@
+import keycloak from "../keycloak";
+import { createHeaders } from "./ApiIndex";
+
+
+export const getGroups = async () => {
+    try {
+        // Refresh token if it is expired or will expire soon
+        if (keycloak.token && keycloak.isTokenExpired()) {
+            await keycloak.updateToken();
+        }
+
+        const response = await fetch("https://localhost:7240/api/v1/Groups", {
+            method: 'GET',
+            headers: createHeaders()
+        });
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
