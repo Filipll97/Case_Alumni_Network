ec2b7e31e869fd1a5c6b1cfafe38f8af56fdb0ed
Filipll97 <filiplindell2@gmail.com>
2023-03-25 16:59:26 +0100

    feat: Added Styling and logic for create Group

--- alumni-network-ui/src/components/Post/SearchBar.jsx
+++ alumni-network-ui/src/components/Post/SearchBar.jsx
@@ -13,17 +13,17 @@ function SearchBar({ onSearch }) {
     };
 
     return (
-        <form onSubmit={handleSearch} className="w-full flex justify-center mb-8">
+        <form onSubmit={handleSearch} className="w-full flex justify-center mb-8 mx-4">
             <input
                 type="text"
                 value={searchInput}
                 onChange={handleInputChange}
                 placeholder="Search posts"
-                className="w-2/3 px-4 py-2 border text-slate-700 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                className="w-full px-4 py-2 border text-slate-700 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             />
             <button
                 type="submit"
-                className="ml-4 px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none hover:bg-blue-600"
+                className="ml-4 px-4 py-2 mr-20 bg-blue-500 text-white rounded-lg focus:outline-none hover:bg-blue-600"
             >
                 Search
             </button>
